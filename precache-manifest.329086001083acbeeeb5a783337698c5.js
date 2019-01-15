f564367a3adda44b304dc8ac791ff1b0b9b0e409
Tom Keschl <tom@keschl.com>
2019-01-14 23:21:47 -0500

    Updates

---
+++ precache-manifest.329086001083acbeeeb5a783337698c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7b77b772eec6b962a3",
+    "url": "/static/css/main.b0bae62f.chunk.css"
+  },
+  {
+    "revision": "8b7b77b772eec6b962a3",
+    "url": "/static/js/main.8b7b77b7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53a15d9aa37893fb52571f00a522ea86",
+    "url": "/index.html"
+  }
+];
